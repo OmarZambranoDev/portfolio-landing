4ee0cdb9b167d76b375807ee1eb7bdfd162cd9f6
Omar Zambrano <omar.zmbrn1@gmail.com>
2026-04-16 18:46:41 -0500

    chore: configure ESLint with Next.js and Prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        earth: {
+          forest: '#344b33',
+          sand: '#b78d6a',
+          stone: '#c5ae96',
+          sage: '#7f886e',
+          moss: '#4a5d3e',
+          clay: '#a07d5c',
+          warm: '#d4bfa8',
+        },
+      },
+    },
   },
   plugins: [],
 };
